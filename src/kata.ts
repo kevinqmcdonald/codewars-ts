507d9cb57b17772d6b72da61419d4bae89585f2e
Kevin McDonald <kevinqmcdonald@gmail.com>
2024-08-04 12:20:24 -0400

    solution

--- src/kata.ts
+++ src/kata.ts
@@ -1,1 +1,14 @@
-export function solution() {}
+export function humanReadable(seconds: number): string {
+    const hoursConversion = convert(seconds, 60 * 60);
+    const minutesConversion = convert(hoursConversion[1], 60);
+    return `${format(hoursConversion[0])}:${format(minutesConversion[0])}:${format(minutesConversion[1])}`;
+}
+
+function convert(seconds: number, factor: number): [number, number] {
+    const converted = Math.floor(seconds * (1 / factor));
+    return [converted, seconds - converted * factor];
+}
+
+function format(value: number): string {
+    return value.toString().padStart(2, '0');
+}
--- test/kata.spec.ts
+++ test/kata.spec.ts
@@ -1,6 +1,16 @@
 import { assert } from 'chai';
-import { solution } from '../src/kata';
+import { humanReadable } from '../src/kata';
 
-describe('Tests', () => {
-    it('should pass all tests', () => {});
+describe('examples', function () {
+    it('should format correctly', function () {
+        assert.equal(humanReadable(0), '00:00:00', 'humanReadable(0)');
+        assert.equal(humanReadable(5), '00:00:05', 'humanReadable(5)');
+        assert.equal(humanReadable(60), '00:01:00', 'humanReadable(60)');
+        assert.equal(humanReadable(86399), '23:59:59', 'humanReadable(86399)');
+        assert.equal(
+            humanReadable(359999),
+            '99:59:59',
+            'humanReadable(359999)'
+        );
+    });
 });
