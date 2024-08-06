8ff9b70b50e85e4189771c7324df5b4c06ebea7a
Kevin McDonald <kevinqmcdonald@gmail.com>
2024-08-06 13:23:39 -0400

    solution

--- src/kata.ts
+++ src/kata.ts
@@ -1,1 +1,33 @@
-export function solution() {}
+const pairs: [string, string][] = [
+    ['NORTH', 'SOUTH'],
+    ['SOUTH', 'NORTH'],
+    ['EAST', 'WEST'],
+    ['WEST', 'EAST']
+];
+
+export function dirReduc(directions: string[]): string[] {
+    let result = [];
+    for (const direction of directions) {
+        result.push(direction);
+        result = reduce(result);
+    }
+
+    return result;
+}
+
+function reduce(directions: string[]): string[] {
+    const result = [...directions];
+    for (let i = 0; i < directions.length - 1; i++) {
+        if (
+            pairs.find(
+                (pair) =>
+                    pair[0] === directions[i] && pair[1] === directions[i + 1]
+            )
+        ) {
+            result.splice(i, 2);
+            return reduce(result);
+        }
+    }
+
+    return result;
+}
--- test/kata.spec.ts
+++ test/kata.spec.ts
@@ -1,6 +1,31 @@
 import { assert } from 'chai';
-import { solution } from '../src/kata';
+import { dirReduc } from '../src/kata';
 
-describe('Tests', () => {
-    it('should pass all tests', () => {});
+describe('Fixed Tests', function () {
+    it('Basic tests', function () {
+        assert.deepEqual(
+            dirReduc([
+                'NORTH',
+                'SOUTH',
+                'SOUTH',
+                'EAST',
+                'WEST',
+                'NORTH',
+                'WEST'
+            ]),
+            ['WEST']
+        );
+        assert.deepEqual(
+            dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH']),
+            []
+        );
+    });
+    it('Advanced tests', function () {
+        assert.deepEqual(dirReduc(['NORTH', 'WEST', 'SOUTH', 'EAST']), [
+            'NORTH',
+            'WEST',
+            'SOUTH',
+            'EAST'
+        ]);
+    });
 });
